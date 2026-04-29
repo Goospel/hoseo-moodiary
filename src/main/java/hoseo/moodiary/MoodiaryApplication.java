1d5353424d3a945211396558ff540466b5385eaa
KimZion <kimsadol369785@gmail.com>
2026-04-29 11:16:08 +0900

    init

---
+++ src/main/java/hoseo/moodiary/MoodiaryApplication.java
@@ -0,0 +1,13 @@
+package hoseo.moodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodiaryApplication.class, args);
+	}
+
+}
