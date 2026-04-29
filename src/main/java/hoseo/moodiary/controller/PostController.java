a55143d3f4d61fbec8aeae2a6d243d6b4b9c2f1f
KimZion <kimsadol369785@gmail.com>
2026-04-29 15:17:35 +0900

    PostContoller.전체조회 메서드 구현

--- src/main/java/hoseo/moodiary/controller/PostController.java
+++ src/main/java/hoseo/moodiary/controller/PostController.java
@@ -4,11 +4,15 @@ import hoseo.moodiary.dto.request.PostRequestDto;
 import hoseo.moodiary.dto.response.PostResponseDto;
 import hoseo.moodiary.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +25,12 @@ public class PostController {
         PostResponseDto responseDto = service.create(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(responseDto);
+    }
+
+    @GetMapping("/post")
+    public ResponseEntity<List<PostResponseDto>> getAll() {
+        List<PostResponseDto> allPosts = service.getAllPosts();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(allPosts);
     }
 }
