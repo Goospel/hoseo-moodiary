b125220f46a5b7aba28eee72c7ea90f3121b1cff
KimZion <kimsadol369785@gmail.com>
2026-04-29 14:58:58 +0900

    PostController.post() 완성

--- src/main/java/hoseo/moodiary/controller/PostController.java
+++ src/main/java/hoseo/moodiary/controller/PostController.java
@@ -1,4 +1,25 @@
 package hoseo.moodiary.controller;
 
+import hoseo.moodiary.dto.request.PostRequestDto;
+import hoseo.moodiary.dto.response.PostResponseDto;
+import hoseo.moodiary.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService service;
+
+    @PostMapping("/post")
+    public ResponseEntity<PostResponseDto> post(@RequestBody PostRequestDto requestDto) {
+        PostResponseDto responseDto = service.create(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(responseDto);
+    }
 }
